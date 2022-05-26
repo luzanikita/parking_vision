4d45e3eb3e10a1c88eebb3dcd23d44cc5e72ac05
Nikita Luzan <36262938+luzanikita@users.noreply.github.com>
2022-05-26 23:30:45 +0300

    Update app.py

--- webapp_backend/app.py
+++ webapp_backend/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from libs.flask_restless import APIManager
@@ -13,7 +15,7 @@ app = Flask(__name__)
 api = Api(app, errors=errors)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://apkjlezxinqwvu:1de60ac67c90127aeb5e86ee85745c573a3cce9a466ef98cb7d52ce0064432c1@ec2-54-216-185-51.eu-west-1.compute.amazonaws.com:5432/djmradro13nfi'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['AUTOCRUD_METADATA_ENABLED'] = True
 app.config['CORS_HEADERS'] = 'Content-Type'
